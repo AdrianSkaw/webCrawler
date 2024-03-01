af8f81b7215813583933047a10c5ab6931880811
Adrian Skawiński <adrian@ipor.io>
2024-03-01 20:13:09 +0100

    modfy repository

--- crawler/models.py
+++ crawler/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 
 class Brand(models.Model):
-    title = models.CharField(max_length=50, blank=False, )
-    url = models.CharField(max_length=50, blank=False)
+    title = models.CharField(max_length=500)
+    url = models.URLField()
 
     def __str__(self):
         return self.title
 
 
 class BrandDetails(models.Model):
-    title = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    price = models.FloatField(blank=False)
-    strike_price = models.FloatField(blank=True, null=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    strike_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
 
     def __str__(self):
-        return self.title.title
+        return self.brand.title
--- crawler/repository.py
+++ crawler/repository.py
@@ -1,48 +1,82 @@
 from crawler.models import Brand, BrandDetails
 from rest_framework.exceptions import NotFound
 
+from crawler.serializer import BrandSerializer, BrandDetailsSerializer
 
-class BaseRepository:
+
+class BrandRepository:
     @staticmethod
-    def create(model_cls, **kwargs):
-        return model_cls.objects.create(**kwargs)
+    def create(product_data):
+        serializer = BrandSerializer(data=product_data)
+        if serializer.is_valid():
+            return serializer.save()
+        else:
+            # Obsługa błędów walidacji
+            pass
 
-    @staticmethod
-    def filter(model_cls, **kwargs):
-        return model_cls.objects.filter(**kwargs)
-
-    @staticmethod
-    def get(model_cls, **kwargs):
-        try:
-            return model_cls.objects.get(**kwargs)
-        except model_cls.DoesNotExist:
-            raise NotFound(f"{model_cls.__name__} not exist")
-
-
-class BrandRepository(BaseRepository):
-    @staticmethod
-    def create(product):
-        return BaseRepository.create(
-            Brand,
-            title=product["title"],
-            url=product["url"]
-        )
+    # @staticmethod
+    # def is_exist(product_data):
+    #     try:
+    #         Brand.objects.get(title=product_data.get("title"))
+    #         return True
+    #     except Brand.DoesNotExist:
+    #         return False
 
     @staticmethod
     def is_exist(product):
-        return BaseRepository.filter(Brand, title=product.get("title"))
+        obj = Brand.objects.filter(title=product.get("title"))
+        return obj
 
     @staticmethod
     def get_by_name(name):
-        return BaseRepository.get(Brand, title=name)
+        try:
+            return Brand.objects.get(title=name)
+        except Brand.DoesNotExist:
+            raise NotFound("Brand not exist")
 
 
-class BrandDetailsRepository(BaseRepository):
+class BrandDetailsRepository:
     @staticmethod
-    def create(brand: Brand, product):
-        return BaseRepository.create(
-            BrandDetails,
-            title=brand,
-            price=product.get("price", -1),
-            strike_price=-1
-        )
+    def create(brand, product_data):
+        product_data['brand'] = brand.id
+        serializer = BrandDetailsSerializer(data=product_data)
+        if serializer.is_valid():
+            return serializer.save()
+        else:
+            # Obsługa błędów walidacji
+            pass
+# from crawler.models import Brand, BrandDetails
+# from rest_framework.exceptions import NotFound
+#
+#
+# class BrandRepository:
+#     @staticmethod
+#     def create(product):
+#         price = product.get('price')
+#         brand = Brand.objects.create(
+#             title=product["title"],
+#             url=product["url"]
+#         )
+#         return brand
+#
+#     @staticmethod
+#     def is_exist(product):
+#         return Brand.objects.filter(title=product.get("title"))
+#
+#     @staticmethod
+#     def get_by_name(name):
+#         try:
+#             brand = Brand.objects.get(title=name)
+#         except Brand.DoesNotExist:
+#             raise NotFound("Brand not exist")
+#         return brand
+#
+#
+# class BrandDetailsRepository:
+#     @staticmethod
+#     def create(brand: Brand, product):
+#         brand_price = BrandDetails.objects.create(
+#             title=brand,
+#             price=product.get("price", -1),
+#             strike_price=-1
+#         )
