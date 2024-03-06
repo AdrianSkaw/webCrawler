6c8c3d3e4a5cccacd41db0fcd89d16b1f779585a
Adrian Skawiński <adrian@ipor.io>
2024-03-06 16:14:15 +0100

    Refactor crawler

--- crawler/admin.py
+++ crawler/admin.py
@@ -1,11 +1,1 @@
-# from django.contrib import admin
-#
-# from crawler.models import Brand, BrandDetails
-#
-#
-# # @admin.register(Brand)
-# class BrandAdmin(admin.ModelAdmin):
-#     list_display = ['title', 'url', 'price']
-#
-#     admin.site.register(Brand)
-#     admin.site.register(BrandDetails)
+
